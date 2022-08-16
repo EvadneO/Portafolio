af1af1438500f47e4520ed65faaef894c23d075d
EvadneO <evadne28@gmail.com>
2022-08-16 16:51:59 +0000

    ajustes

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,46 +5,48 @@ import eva from "../../img/eva.jpeg";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-info">
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide"
-        data-bs-ride="false"
-      >
-        <div className="carousel-indicators"></div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src="https://www.nikstech.com/images/resource/2.gif"
-              className="img-fluid"
-              style={{ width: "100rem" }}
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block">
+    <div className="float">
+      <nav className="navbar navbar-light bg-info">
+        <div
+          id="carouselExampleCaptions"
+          className="carousel slide"
+          data-bs-ride="false"
+        >
+          <div className="carousel-indicators"></div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
               <img
-                src={eva}
-                className="rounded-circle"
-                style={{ width: "8rem" }}
-              ></img>
-              <h1
-                className="display-1 "
-                style={{
-                  fontFamily: "Trebuchet MS",
-                  fontWeight: "extrabold",
-                }}
-              >
-                Evadne Olivo
-              </h1>
-              <div className="linea"></div>
-              <h3>
-                <strong style={{ fontFamily: "Trebuchet MS" }}>
-                  Desarrolladora Full Stack
-                </strong>
-              </h3>
+                src="https://www.nikstech.com/images/resource/2.gif"
+                className="img-fluid"
+                style={{ width: "130rem" }}
+                alt="..."
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <img
+                  src={eva}
+                  className="rounded-circle"
+                  style={{ width: "8rem" }}
+                ></img>
+                <h1
+                  className="display-1 "
+                  style={{
+                    fontFamily: "Trebuchet MS",
+                    fontWeight: "extrabold",
+                  }}
+                >
+                  Evadne Olivo
+                </h1>
+                <div className="linea"></div>
+                <h3>
+                  <strong style={{ fontFamily: "Trebuchet MS" }}>
+                    Desarrolladora Full Stack
+                  </strong>
+                </h3>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -24,161 +24,67 @@ export const Home = () => {
   }, []);
 
   return (
-    <div className="fluid">
-      <nav className="navbar navbar-light bg-info">
-        <div className="container-fluid">
-          <div className="navbar-brand">
-            <h5 style={{ fontFamily: "sans-serif" }}>Evadne Olivo </h5>{" "}
-            <h6 className="text-muted" style={{ fontFamily: "serif" }}>
-              Desarrolladora Full Stack
-            </h6>
-          </div>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav text-center">
-              <h6>
-                <a
-                  className="nav-link active text-white"
-                  onClick={() => clickHandler("portafolio")}
-                >
-                  Portafolio
-                </a>
-              </h6>
-              <h6>
-                <a
-                  className="nav-link text-white"
-                  onClick={() => clickHandler("tecnologia")}
-                >
-                  Tecnologías
-                </a>
-              </h6>
-              <h6>
-                <a
-                  className="nav-link text-white"
-                  onClick={() => clickHandler("experiencia")}
-                >
-                  Experiencia
-                </a>
-              </h6>
-              <h6>
-                <a
-                  className="nav-link text-white"
-                  onClick={() => clickHandler("contacto")}
-                >
-                  Contacto
-                </a>
+    <div className="float">
+      <div className="fluid">
+        <nav className="navbar navbar-light bg-info">
+          <div className="container-fluid">
+            <div className="navbar-brand">
+              <h5 style={{ fontFamily: "sans-serif" }}>Evadne Olivo </h5>{" "}
+              <h6 className="text-muted" style={{ fontFamily: "serif" }}>
+                Desarrolladora Full Stack
               </h6>
             </div>
-          </div>
-        </div>
-      </nav>
-      {/* Detalle de Portafolio */}
-      {mode === "portafolio" ? (
-        <div className="container">
-          <div className="text-center">
-            <h1
-              className="display-4"
-              style={{
-                fontFamily: "Trebuchet MS",
-                fontWeight: "extrabold",
-              }}
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              Portafolio
-            </h1>
-            <div className="linea"></div>
-          </div>
-
-          <div className="row mt-5">
-            {store.proyectos.map(function (objeto) {
-              return <Portafolio objeto={objeto} />;
-            })}
-          </div>
-        </div>
-      ) : mode === "tecnologia" ? (
-        <div className="text-center bg-info">
-          <h1
-            className="display-4"
-            style={{
-              fontFamily: "Trebuchet MS",
-              fontWeight: "extrabold",
-              color: "white",
-            }}
-          >
-            Tecnologías
-          </h1>
-          <div className="lineas"></div>
-          <div className="row">
-            <div
-              className="row"
-              style={{
-                margin: "auto",
-                marginBottom: "8rem",
-                marginTop: "3rem",
-              }}
-            >
-              <Tecnologia />
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav text-center">
+                <h6>
+                  <a
+                    className="nav-link active text-white"
+                    onClick={() => clickHandler("portafolio")}
+                  >
+                    Portafolio
+                  </a>
+                </h6>
+                <h6>
+                  <a
+                    className="nav-link text-white"
+                    onClick={() => clickHandler("tecnologia")}
+                  >
+                    Tecnologías
+                  </a>
+                </h6>
+                <h6>
+                  <a
+                    className="nav-link text-white"
+                    onClick={() => clickHandler("experiencia")}
+                  >
+                    Experiencia
+                  </a>
+                </h6>
+                <h6>
+                  <a
+                    className="nav-link text-white"
+                    onClick={() => clickHandler("contacto")}
+                  >
+                    Contacto
+                  </a>
+                </h6>
+              </div>
             </div>
           </div>
-        </div>
-      ) : mode === "experiencia" ? (
-        <div className="container">
-          <div className="text-center">
-            <h1
-              className="display-4"
-              style={{
-                fontFamily: "Trebuchet MS",
-                fontWeight: "extrabold",
-              }}
-            >
-              Experiencia
-            </h1>
-            <div className="linea"></div>
-          </div>
-
-          <div className="row mt-5">
-            {store.experience.map(function (objeto) {
-              return <Experiencia objeto={objeto} />;
-            })}
-          </div>
-        </div>
-      ) : mode === "contacto" ? (
-        <div className="text-center bg-info">
-          <h1
-            className="display-4"
-            style={{
-              fontFamily: "Trebuchet MS",
-              fontWeight: "extrabold",
-              color: "white",
-            }}
-          >
-            Contact
-          </h1>
-          <div className="lineas"></div>
-          <div className="row">
-            <div
-              className="col-6"
-              style={{
-                margin: "auto",
-                marginBottom: "10rem",
-                marginTop: "5rem",
-              }}
-            >
-              <Contacto />
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="fluid">
+        </nav>
+        {/* Detalle de Portafolio */}
+        {mode === "portafolio" ? (
           <div className="container">
             <div className="text-center">
               <h1
@@ -199,6 +105,7 @@ export const Home = () => {
               })}
             </div>
           </div>
+        ) : mode === "tecnologia" ? (
           <div className="text-center bg-info">
             <h1
               className="display-4"
@@ -224,24 +131,28 @@ export const Home = () => {
               </div>
             </div>
           </div>
-          <div className="text-center">
-            <h1
-              className="display-4"
-              style={{
-                fontFamily: "Trebuchet MS",
-                fontWeight: "extrabold",
-              }}
-            >
-              Experience
-            </h1>
-            <div className="linea"></div>
-          </div>
-          <div className="row" style={{ margin: "auto" }}>
-            {store.experience.map(function (objeto) {
-              return <Experiencia objeto={objeto} />;
-            })}
-          </div>
+        ) : mode === "experiencia" ? (
+          <div className="container">
+            <div className="text-center">
+              <h1
+                className="display-4"
+                style={{
+                  fontFamily: "Trebuchet MS",
+                  fontWeight: "extrabold",
+                }}
+              >
+                Experiencia
+              </h1>
+              <div className="linea"></div>
+            </div>
 
+            <div className="row mt-5">
+              {store.experience.map(function (objeto) {
+                return <Experiencia objeto={objeto} />;
+              })}
+            </div>
+          </div>
+        ) : mode === "contacto" ? (
           <div className="text-center bg-info">
             <h1
               className="display-4"
@@ -267,8 +178,99 @@ export const Home = () => {
               </div>
             </div>
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="fluid">
+            <div className="container">
+              <div className="text-center">
+                <h1
+                  className="display-4"
+                  style={{
+                    fontFamily: "Trebuchet MS",
+                    fontWeight: "extrabold",
+                  }}
+                >
+                  Portafolio
+                </h1>
+                <div className="linea"></div>
+              </div>
+
+              <div className="row mt-5">
+                {store.proyectos.map(function (objeto) {
+                  return <Portafolio objeto={objeto} />;
+                })}
+              </div>
+            </div>
+            <div className="text-center bg-info">
+              <h1
+                className="display-4"
+                style={{
+                  fontFamily: "Trebuchet MS",
+                  fontWeight: "extrabold",
+                  color: "white",
+                }}
+              >
+                Tecnologías
+              </h1>
+              <div className="lineas"></div>
+              <div className="row">
+                <div
+                  className="row"
+                  style={{
+                    margin: "auto",
+                    marginBottom: "8rem",
+                    marginTop: "3rem",
+                  }}
+                >
+                  <Tecnologia />
+                </div>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1
+                className="display-4"
+                style={{
+                  fontFamily: "Trebuchet MS",
+                  fontWeight: "extrabold",
+                }}
+              >
+                Experience
+              </h1>
+              <div className="linea"></div>
+            </div>
+            <div className="row" style={{ margin: "auto" }}>
+              {store.experience.map(function (objeto) {
+                return <Experiencia objeto={objeto} />;
+              })}
+            </div>
+
+            <div className="text-center bg-info">
+              <h1
+                className="display-4"
+                style={{
+                  fontFamily: "Trebuchet MS",
+                  fontWeight: "extrabold",
+                  color: "white",
+                }}
+              >
+                Contact
+              </h1>
+              <div className="lineas"></div>
+              <div className="row">
+                <div
+                  className="col-6"
+                  style={{
+                    margin: "auto",
+                    marginBottom: "10rem",
+                    marginTop: "5rem",
+                  }}
+                >
+                  <Contacto />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
